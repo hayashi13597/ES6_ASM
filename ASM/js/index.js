faa316adad7a4b110ab33344d4c1621333a91faa
hayashi13597 <hpl13597@gmail.com>
2023-02-15 13:15:05 +0700

    asm

--- ASM/js/index.js
+++ ASM/js/index.js
@@ -41,8 +41,8 @@ window.onscroll = function () {
 function scrollFunction() {
   let mybutton = document.getElementById("myBtn");
   if (
-    document.body.scrollTop > 200 ||
-    document.documentElement.scrollTop > 200
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
   ) {
     mybutton.classList.remove("disabled");
     showCart.classList.remove("js-action-show");
@@ -60,8 +60,8 @@ function fixedHeader() {
   const headerFixed = document.getElementById("header-fixed");
   const navFixed = document.getElementById("nav-fixed");
   if (
-    document.body.scrollTop > 200 ||
-    document.documentElement.scrollTop > 200
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
   ) {
     headerFixed.classList.add("header-fixed");
     navFixed.classList.add("header-nav__fixed");
@@ -70,6 +70,82 @@ function fixedHeader() {
     navFixed.classList.remove("header-nav__fixed");
   }
 }
+const showAllCart = () => {
+  const cart = document.getElementById("mini-cart");
+  const cartCount = document.querySelector(".cart__number");
+  const products = JSON.parse(sessionStorage.getItem("cart"));
+  let sum = 0;
+  if (products) {
+    for (const el of products) {
+      let gia = el.price.split(",").join("");
+      sum += parseInt(gia) * parseInt(el.count);
+    }
+    const formatter = new Intl.NumberFormat("en-US", {
+      currency: "USD",
+    });
+    sum = formatter.format(sum);
+  }
+  if (products == null || products.length == 0) {
+    cart.innerHTML = `<tr class="mini-cart__empty">
+    <td>
+      <div class="svgico-mini-cart">
+        <svg
+          width="81"
+          height="70"
+          viewBox="0 0 32 32"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <g id="icomoon-ignore"></g>
+          <path
+            d="M30.622 9.602h-22.407l-1.809-7.464h-5.027v1.066h4.188l5.198 21.443c-1.108 0.323-1.923 1.334-1.923 2.547 0 1.472 1.193 2.666 2.666 2.666s2.666-1.194 2.666-2.666c0-0.603-0.208-1.153-0.545-1.599h7.487c-0.337 0.446-0.545 0.997-0.545 1.599 0 1.472 1.193 2.666 2.665 2.666s2.666-1.194 2.666-2.666c0-1.473-1.193-2.665-2.666-2.666v0h-11.403l-0.517-2.133h14.968l4.337-12.795zM13.107 27.196c0 0.882-0.717 1.599-1.599 1.599s-1.599-0.717-1.599-1.599c0-0.882 0.717-1.599 1.599-1.599s1.599 0.718 1.599 1.599zM24.836 27.196c0 0.882-0.718 1.599-1.6 1.599s-1.599-0.717-1.599-1.599c0-0.882 0.717-1.599 1.599-1.599 0.882 0 1.6 0.718 1.6 1.599zM11.058 21.331l-2.585-10.662h20.662l-3.615 10.662h-14.462z"
+            fill="#000000"
+          ></path>
+        </svg>
+      </div>
+      Hiện chưa có sản phẩm
+    </td>
+  </tr>`;
+    cartCount.innerText = 0;
+    document.getElementById("total-view-cart").innerText = "0đ";
+  } else {
+    const tbody = products
+      .map((product) => {
+        return `<tr class="mini-cart__item">
+      <td class="mini-cart__left">
+        <a href="#">
+          <img
+            src="${product.img}"
+            alt=""
+          />
+        </a>
+      </td>
+      <td class="mini-cart__right">
+        <p class="mini-cart__title">
+          <a href="#">${product.name}</a>
+        </p>
+        <div class="mini-cart__quantity">
+          <span class="mnc-value">${product.count}</span>
+        </div>
+        <div class="mini-cart__price">
+          <span class="mnc-price">${product.price}đ</span>
+        </div>
+        <div class="mini-cart__remove">
+          <a href="#" onclick="event.preventDefault();deleteProduct(this)" data="${product.id}" id="btn-delete">
+          <i class="fa-solid fa-xmark"></i>
+          </a>
+        </div>
+      </td>
+    </tr>`;
+      })
+      .join("");
+    document.getElementById("mini-cart").innerHTML = tbody;
+    cartCount.innerHTML = products.length;
+    document.getElementById("total-view-cart").innerText = sum + "đ";
+  }
+};
+showAllCart();
 const deleteProduct = (e) => {
   const dataId = e.getAttribute("data");
   let products = JSON.parse(sessionStorage.getItem("cart"));
